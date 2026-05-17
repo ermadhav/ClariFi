7413f6d22581260dc254efca1bb435477af408a6
Cosmo Coder <madhavtiwari2024@gmail.com>
2026-05-17 13:20:49 +0530

    Syncing the broker data to web pages

---
+++ test_screener.js
@@ -0,0 +1,9 @@
+const fetch = require('node-fetch'); // NextJS uses fetch globally, node uses global fetch in >=18
+async function test() {
+  const r = await fetch('https://www.screener.in/company/RELIANCE/consolidated/');
+  const html = await r.text();
+  const peMatch = html.match(/Stock P\/E.*?<span class="number">([^<]+)<\/span>/s);
+  const hlMatch = html.match(/High \/ Low.*?<span class="number">([^<]+)<\/span>.*?<span class="number">([^<]+)<\/span>/s);
+  console.log('PE:', peMatch?.[1], 'HL:', hlMatch?.[1], hlMatch?.[2]);
+}
+test();
